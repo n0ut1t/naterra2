7fe2f7006216cdff07ac28f01c5e86679677d46d
Iker <alu.iker.blanco@mataro.epiaedu.cat>
2026-01-12 16:15:27 +0100

    conexion a db supabase

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -4,17 +4,17 @@
 ])
 
 @section('content')
-    <div id="ch1" class="chapter"><a href="{{ route('nivells') }}">1</a></div>
+    <div id="ch1" class="chapter"><a href="{{ route('nivells') }}" style="text-decoration:none">1</a></div>
     <div id="ch2" class="chapter">
-        <a href="{{ route('pregunta', [2, 1]) }}">2</a>
+        <a href="{{ route('pregunta', [2, 1]) }}" style="text-decoration:none" >2</a>
     </div>
     <div id="ch3" class="chapter">
-        <a href="{{ route('pregunta', [3, 1]) }}">3</a>
+        <a href="{{ route('pregunta', [3, 1]) }}" style="text-decoration:none">3</a>
     </div>
     <div id="ch4" class="chapter">
-        <a href="{{ route('pregunta', [4, 1]) }}">4</a>
+        <a href="{{ route('pregunta', [4, 1]) }}" style="text-decoration:none">4</a>
     </div>
     <div id="ch5" class="chapter">
-        <a href="{{ route('pregunta', [5, 1]) }}">5</a>
+        <a href="{{ route('pregunta', [5, 1]) }}" style="text-decoration:none">5</a>
     </div>
 @endsection
--- resources/views/nivells.blade.php
+++ resources/views/nivells.blade.php
@@ -3,16 +3,16 @@
 ])
 
 @section('content')
-    <div id="p1" class="chapter"><a href="{{ route('pregunta', [1, 1]) }}">1</a></div>
+    <div id="p1" class="chapter"><a href="{{ route('pregunta', [1, 1]) }}" style="text-decoration:none">1</a></div>
 
-    <div id="p2" class="chapter"><a href="{{ route('pregunta', [1, 2]) }}">2</a></div>
-    <div id="p3" class="chapter"><a href="{{ route('pregunta', [1, 3]) }}">3</a></div>
-    <div id="p4" class="chapter"><a href="{{ route('pregunta', [1, 4]) }}">4</a></div>
-    <div id="p5" class="chapter"><a href="{{ route('pregunta', [1, 5]) }}">5</a></div>
-    <div id="p6" class="chapter"><a href="{{ route('pregunta', [1, 6]) }}">6</a></div>
-    <div id="p7" class="chapter"><a href="{{ route('pregunta', [1, 7]) }}">7</a></div>
-    <div id="p8" class="chapter"><a href="{{ route('pregunta', [1, 8]) }}">8</a></div>
-    <div id="p9" class="chapter"><a href="{{ route('pregunta', [1, 9]) }}">9</a></div>
-    <div id="p10" class="chapter"><a href="{{ route('pregunta', [1, 10]) }}">10</a></div>
+    <div id="p2" class="chapter"><a href="{{ route('pregunta', [1, 2]) }}" style="text-decoration:none">2</a></div>
+    <div id="p3" class="chapter"><a href="{{ route('pregunta', [1, 3]) }}" style="text-decoration:none">3</a></div>
+    <div id="p4" class="chapter"><a href="{{ route('pregunta', [1, 4]) }}" style="text-decoration:none">4</a></div>
+    <div id="p5" class="chapter"><a href="{{ route('pregunta', [1, 5]) }}" style="text-decoration:none">5</a></div>
+    <div id="p6" class="chapter"><a href="{{ route('pregunta', [1, 6]) }}" style="text-decoration:none">6</a></div>
+    <div id="p7" class="chapter"><a href="{{ route('pregunta', [1, 7]) }}" style="text-decoration:none">7</a></div>
+    <div id="p8" class="chapter"><a href="{{ route('pregunta', [1, 8]) }}" style="text-decoration:none">8</a></div>
+    <div id="p9" class="chapter"><a href="{{ route('pregunta', [1, 9]) }}" style="text-decoration:none">9</a></div>
+    <div id="p10" class="chapter"><a href="{{ route('pregunta', [1, 10]) }}" style="text-decoration:none"   >10</a></div>
 @endsection
 
