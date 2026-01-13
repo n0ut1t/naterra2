75daa08f1e7e3de882ac375fb9ecf43e9b2f441f
Iker <alu.iker.blanco@mataro.epiaedu.cat>
2026-01-13 17:33:30 +0100

    Conexion supabase(leer README)

--- config/database.php
+++ config/database.php
@@ -73,7 +73,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
 
